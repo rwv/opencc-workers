939edc0354824524117fd72c9d532f14d04bdd55
Seedgou <i@zczc.cz>
2024-05-12 23:43:33 +0800

    feat: remove unused task-related endpoints and types

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,10 @@
 import { OpenAPIRouter } from "@cloudflare/itty-router-openapi";
-import { TaskCreate } from "./endpoints/taskCreate";
-import { TaskDelete } from "./endpoints/taskDelete";
-import { TaskFetch } from "./endpoints/taskFetch";
-import { TaskList } from "./endpoints/taskList";
 import { Translator } from "./endpoints/translator";
 
 export const router = OpenAPIRouter({
 	docs_url: "/",
 });
 
-router.get("/api/tasks/", TaskList);
-router.post("/api/tasks/", TaskCreate);
-router.get("/api/tasks/:taskSlug/", TaskFetch);
-router.delete("/api/tasks/:taskSlug/", TaskDelete);
 router.get("/translate", Translator);
 
 // 404 for everything else
